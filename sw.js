43faf1a4ff9f84a727b9e7b66da18203d754757e
grasyagrasya99 <g.ta-oy.561524@umindanao.edu.ph>
2025-08-24 13:19:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,14 +9,12 @@ const urlsToCache = [
   "https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg"
 ];
 
-// Install the service worker and cache files
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activate and remove old caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) =>
@@ -29,7 +27,6 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// Serve cached content when offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
